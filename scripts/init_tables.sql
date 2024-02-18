cf5cd21dad3c9e9cc6e1c0718ce51fa96fddf33a
Philip Berner <philipberner.berlin@gmail.com>
2024-02-18 16:11:48 +0100

    added data

---
+++ scripts/init_tables.sql
@@ -0,0 +1,71 @@
+/**
+  DROP TABLES IF THEY EXIT
+ */
+DROP TABLE SELECTED;
+DROP TABLE EXECUTION_TIME;
+DROP TABLE PERSON_DATA_A;
+DROP TABLE PERSON_DATA_B;
+DROP TABLE PERSON_DATA_C;
+DROP TABLE PERSON_DATA_D;
+
+/*
+  Create TABLES
+ */
+CREATE TABLE SELECTED
+(
+    name       varchar(50),
+    first_name varchar(50),
+    postcode   varchar(5),
+    city       varchar(50),
+    country    varchar(50),
+    occupation varchar(100)
+);
+
+CREATE TABLE EXECUTION_TIME
+(
+    parallel   boolean,
+    start_time timestamp,
+    end_time   timestamp
+);
+
+CREATE TABLE PERSON_DATA_A
+(
+    name       varchar(50),
+    first_name varchar(50),
+    postcode   varchar(5),
+    city       varchar(50),
+    country    varchar(50),
+    occupation varchar(100)
+);
+
+CREATE TABLE PERSON_DATA_B
+(
+    name       varchar(50),
+    first_name varchar(50),
+    postcode   varchar(5),
+    city       varchar(50),
+    country    varchar(50),
+    occupation varchar(100)
+);
+
+CREATE TABLE PERSON_DATA_C
+(
+    name       varchar(50),
+    first_name varchar(50),
+    postcode   varchar(5),
+    city       varchar(50),
+    country    varchar(50),
+    occupation varchar(100)
+);
+
+CREATE TABLE PERSON_DATA_D
+(
+    name       varchar(50),
+    first_name varchar(50),
+    postcode   varchar(5),
+    city       varchar(50),
+    country    varchar(50),
+    occupation varchar(100)
+);
+
+COMMIT;
